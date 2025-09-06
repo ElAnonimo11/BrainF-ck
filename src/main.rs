3d4239c5e2456d6abb86f6668eae50dfc5bd4872
ElAnonimo11 <oscarortegarios@gmail.com>
2025-09-06 12:55:07 +0200

    Opening the file and saving the data

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-
+use std::fs::read_to_string;
 
 fn main() {
     // We collect the data
@@ -9,5 +9,22 @@ fn main() {
         panic!("Wrong number of arguments");
     }
 
+    // Name of the archive
+    let archive_name: String = v_args[1].clone();
+
+    // Wrong extension
+    if !archive_name.ends_with(".bf") {
+        panic!("Wrong archive extension");
+    }
+
+    // Save the string of data
+    let archive_data: String = read_to_string(archive_name).expect("Error opening the file.");
+
+    // We save only the data that is part of brainfuck, and we save it in a vector for easy index.
+    let v_data: Vec<char> = archive_data.chars().filter(|&e| [',',',','+','-','<','>'].contains(&e)).collect();
+
+    // The index that we are in the vector and the vector
+    let mut index: usize = 0;
+    let mut v_nums: Vec<u8> = vec![0];
 
 }
